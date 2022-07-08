11583d74de2567932b74b3ec2714e1c8db358e76
Anastasia Rubashkina <anastasia.rubashkina@gmail.com>
2022-07-08 13:27:36 +0300

    modified src/index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,10 @@ export default function universalGame(intro, round) {
   console.log(`Hello, ${name}!`);
   console.log(intro);
   for (let i = 0; i < 3; i += 1) {
-    if (round() !== 'correct') { break; }
+    if (round() !== 'correct') {
+        console.log(`Let's try again, ${name}!`);
+        break;
+      }
     if (i === 2) { console.log(`Congratulations, ${name}!`); }
   }
 }
